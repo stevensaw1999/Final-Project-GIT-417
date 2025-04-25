9b138aec2e445bdb6f0fe034bed951c489651df0
Steven <ssaw1@asu.edu>
2025-04-25 08:37:06 -0700

    Update java.js

--- java.js
+++ java.js
@@ -1,1 +1,46 @@
 "use strict"; // uses strict mode globally
+
+// Start of Product Display
+const products = [
+    {
+      name: "House-1",
+      image: "",
+      description: ""
+    },
+    {
+      name: "House-2",
+      image: "",
+      description: ""
+    },
+    {
+      name: "House-3",
+      image: "",
+      description: ""
+    }
+  ];
+
+  // Switching 
+  function houseSwitch(){
+    const buttons = document.querySelectorAll("#switcherButtons button");
+    const productSections = document.querySelectorAll("#switcher1 > section");
+
+    buttons.forEach((button, index) => {
+        button.addEventListener("click", () => {
+            // Hide all product sections
+            productSections.forEach(section => {
+                section.classList.add("hiddenItem");
+                section.classList.remove("currentItem");
+            });
+
+            // Show the selected product
+            const selectedProduct = productSections[index];
+            selectedProduct.classList.remove("hiddenItem");
+            selectedProduct.classList.add("currentItem");
+        });
+    });
+}
+
+// Start of Guessing Game
+
+// Event Handlers
+// for hide show text function
